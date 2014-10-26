34ed42b20ad499cc01708bbbc719b78ff98460de
Daisuke Hashimoto <dsk.hashimoto@gmail.com>
2014-10-26 12:15:31 +0900

    課金ページの途中

--- src/scene/common/promotion_s.js
+++ src/scene/common/promotion_s.js
@@ -2,6 +2,8 @@
 var PromotionSController = cc.Class.extend({
     root: null,
     promotion_layer: null,
+    field: null,
+    is_share_appear: false,
 
     init:function (root) {
         this.root = root;
@@ -12,6 +14,9 @@ var PromotionSController = cc.Class.extend({
         // ボタン制御
         var button_promotion_s_close = ccui.helper.seekWidgetByName(this.promotion_layer, "button_promotion_s_close");
         button_promotion_s_close.addTouchEventListener(this.buttonPromotionSCloseTouchEvent, this);
+
+        var button_get_by_share = ccui.helper.seekWidgetByName(this.promotion_layer, "button_get_by_share");
+        button_get_by_share.addTouchEventListener(this.buttonGetByShareTouchEvent, this);
     },
     buttonPromotionSCloseTouchEvent: function (sender, type) {
         switch (type) {
@@ -21,12 +26,38 @@ var PromotionSController = cc.Class.extend({
             break;
         }
     },
-    appear: function (title, section_title, text) {
+    appear: function (title, field) {
+        this.field = field;
+
+        this.promotion_layer.y = -720;
+
         // 親レイヤにモーダル用のレイヤを追加
         this.root.addChild(this.promotion_layer);
+
+        var move = cc.moveTo(1, cc.p(0, 0)).easing(cc.easeElasticOut());
+        this.promotion_layer.runAction(cc.sequence(move));
     },
     disappear: function () {
-        this.root.removeChild(this.promotion_layer, true);
+        var move = cc.moveTo(1, cc.p(0, -720)).easing(cc.easeElasticOut());
+        var that = this;
+        var callback = cc.callFunc(function(){
+            that.root.removeChild(that.promotion_layer);
+        });
+        this.promotion_layer.runAction(cc.sequence(move, callback));
+    },
+    buttonGetByShareTouchEvent: function (sender, type) {
+        switch (type) {
+        case ccui.Widget.TOUCH_ENDED:
+            cc.log(sender.getName() + " >>> ccui.Widget.TOUCH_ENDED");
+            if(this.is_share_appear){
+                ccs.actionManager.playActionByName(res.UiPromotionS_json, "share_in");
+            }else{
+                ccs.actionManager.playActionByName(res.UiPromotionS_json, "share_out");
+            }
+            this.is_share_appear = !this.is_share_appear;
+            cc.log(this.is_share_appear);
+            break;
+        }
     }
 });
 
